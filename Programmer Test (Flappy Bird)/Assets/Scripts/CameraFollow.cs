5ff9440d515e38ca293816a8c5f872cf2c1f896c
Aaron Drinkwater <Aaron3.Drinkwater@live.uwe.ac.uk>
2021-02-25 17:55:43 +0000

    Follow Player Camera fix, Camera is less buggy on the Y Axis now

--- Programmer Test (Flappy Bird)/Assets/Scripts/CameraFollow.cs
+++ Programmer Test (Flappy Bird)/Assets/Scripts/CameraFollow.cs
@@ -8,13 +8,28 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
     private Vector3 vel;
     public float smoothTime;
+    //public float moveForce;
+
+    //private Rigidbody rb;
+    //public float upwardsForce;
 
     void Start()
     {
-        
+        //rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
         transform.position = Vector3.SmoothDamp(transform.position, player.position + offset, ref vel, smoothTime);
+
+        Vector3 position = transform.position;
+        position.y = (player.position + offset).y;
+        transform.position = position;
+
+        //transform.rotation = player.transform.rotation;
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    rb.AddForce(new Vector3(0, upwardsForce));
+        //}
     }
 }
