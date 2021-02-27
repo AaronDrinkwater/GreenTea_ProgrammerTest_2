5cce026587b81a2b9a7572f5446843de10c24a11
Aaron Drinkwater <Aaron3.Drinkwater@live.uwe.ac.uk>
2021-02-27 02:02:06 +0000

    Attempt to add a coin object to the game.

---
+++ Programmer Test (Flappy Bird)/Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
+            GameControl.instance.ScoreUp();
+        }
+    }
+}
---
+++ Programmer Test (Flappy Bird)/Assets/Scripts/CoinPooling.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPooling : MonoBehaviour
+{
+    public GameObject coinPrefab;
+    private GameObject[] coins;
+    public Transform coinTransform;
+
+    public int coinPoolingSize = 5;
+    private int currentCoin = 0;
+
+    private float spawnTimer;
+    public float spawnRate = 5f;
+    public float coinMin = 2f;
+    public float coinMax = 5f;
+
+    public float spawnXPosition = 0;
+
+    private Vector3 objectPoolingPosition = new Vector3(-15f, -30f, -25f);
+
+    void Start()
+    {
+        coins = new GameObject[coinPoolingSize];
+
+        for (int i = 0; i < coinPoolingSize; i++)
+        {
+            coins[i] = GameObject.Instantiate(coinPrefab, objectPoolingPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (GameControl.instance.isGameOver == false && spawnTimer >= spawnRate)
+        {
+            spawnTimer = 0;
+            float spawnYPosition = Random.Range(coinMin, coinMax);
+
+            coins[currentCoin].transform.position = new Vector3(spawnXPosition, spawnYPosition, 0);
+            currentCoin++;
+
+            if (currentCoin >= coinPoolingSize)
+            {
+                currentCoin = 0;
+            }
+        }
+    }
+}
